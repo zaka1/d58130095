4d033d0f4dee665ccc361afdfda3772567c36921
zaka1 <zaka412229@pchome.com.tw>
2016-10-16 16:23:41 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-import os
-from flask import Flask, render_template, redirect, url_for, flash,request
+from flask import Flask
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1> this is a test! </h1>"
+    return "<H1> this is a test! </H1>"
 if __name__ == '__main__':
     app.run()
